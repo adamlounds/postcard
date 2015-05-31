e7e3c382827379a6c42e68608596bcef1bf681d6
adam lounds <adamlounds@gmail.com>
2015-05-31 14:33:43 +0100

    Woo, Xcode with built in committing

--- Postcard/ViewController.swift
+++ Postcard/ViewController.swift
@@ -37,6 +37,7 @@ class ViewController: UIViewController {
     enterMessageTextField.resignFirstResponder()
     mailButton.setTitle("Mail Sent", forState: UIControlState.Normal)
     // extra line to practice git workflow
+    // another comment following along
     
   }
   
